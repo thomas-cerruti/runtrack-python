fd199eda3e46955f6d74b5961d14287909b6b4fd
thomas-cerruti <thomas.cerruti@laplateforme.io>
2023-11-22 15:39:25 +0100

    Positif Négatif

---
+++ Jour03/Job06/NumPositif.py
@@ -0,0 +1,10 @@
+def test_positif_negatif(nombre):
+    if nombre > 0:
+        print("positif")
+    elif nombre < 0:
+        print("negatif")
+        
+test_positif_negatif(5)
+test_positif_negatif(-8)
+test_positif_negatif(525494654646549618)
+test_positif_negatif(-55494949494984)
