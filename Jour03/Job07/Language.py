4370e3c631ae0ae42b20cd5d9417c5c07343209d
thomas-cerruti <thomas.cerruti@laplateforme.io>
2023-11-22 15:40:29 +0100

    Language

---
+++ Jour03/Job07/Language.py
@@ -0,0 +1,14 @@
+def tu_es(langage):
+    if langage == "JavaScript":
+        print("tu es développeur web")
+    elif langage == "python":
+        print("tu es un développeur IA")
+    elif langage == "java":
+        print("tu es un développeur logiciel")
+    elif langage == "reactjs":
+        print("tu es un developpeur mobile")
+    else:
+        print("un jour, je serai le meilleur développeur...")
+        
+tu_es("lol")
+tu_es("java")
