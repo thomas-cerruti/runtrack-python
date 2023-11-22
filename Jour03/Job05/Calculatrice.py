f169fea700956163d4cb0d8b1902d0850a30e666
thomas-cerruti <thomas.cerruti@laplateforme.io>
2023-11-22 13:38:26 +0100

    Calculatrice

---
+++ Jour03/Job05/Calculatrice.py
@@ -0,0 +1,13 @@
+def calcul(num1, operator, num2):
+    if operator == "+":
+        print(num1 + num2)
+    elif operator == "-":
+        print(num1 - num2)
+    elif operator == "*":
+        print(num1 * num2)
+    elif operator == "/":
+        print(num1 / num2)
+    
+    return print
+
+calcul(9,"*",9)
