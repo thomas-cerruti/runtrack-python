31327f909d80e96677708fcb45e25db21ea1d4b5
thomas-cerruti <thomas.cerruti@laplateforme.io>
2023-11-22 16:18:48 +0100

    Liste

---
+++ Jour03/Job08/Liste.py
@@ -0,0 +1,9 @@
+def liste(type, saison):
+    fruits = {"ete": "Poire, fraise, cassis", "hiver": "orange, mandarine et kiwi"}
+    legumes = {"ete": "artichaut, aubergine,navet", "hiver": "carotte, topinambour, endive"}
+    if type == "fruits":
+        print(fruits[saison])
+    elif type == "legume":
+        print(legumes[saison])
+        
+liste("fruits", "ete")
