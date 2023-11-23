aae7e11ad82d5b3fb85d01ecd2537575258f03a7
thomas-cerruti <thomas.cerruti@laplateforme.io>
2023-11-23 14:05:45 +0100

    job05

---
+++ Jour04/Job05/index.py
@@ -0,0 +1,24 @@
+def chiffre():
+    L = [1, 2, 3, 4, 5]
+    L[3] = L[2] + L[4]
+    print(L[1])
+    print(L)
+    print(L[4])
+
+chiffre()
+
+# L = [1, 2, 3, 4, 5]
+# def chiffre():
+#     L[3] = L[2] + L[4]
+# print(L[1])
+# print(L)
+# print(L[4])
+
+
+# def chiffre():
+#     L = [1, 2, 3, 4, 5]
+#     L[3] = L[2] + L[4]
+#     return L
+# print(chiffre()[1])
+# print(chiffre())
+# print(chiffre()[4])
